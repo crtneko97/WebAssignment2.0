2b1d415b20489d020a4da3d680265161140c09a8
Simon <99965683+crtneko97@users.noreply.github.com>
2023-01-11 22:22:01 +0100

    added some javascript code so that after sending in the form it will display none the form for 3 sec show a paragraph and then reload the side

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const form = document.querySelector("#form");
 
-const submitFunc = () => {
-    alert("Message Sent! We will contact you soon!")
+
+const p = document.getElementById("sentTxt");
+const submitBtn = document.getElementById("submitButton");
+
+
+const removeForm = () => {
+    form.remove();
+    p.innerHTML = "Sent! <br> We will message u shortly";
+    
+    setTimeout(() =>{
+        location.reload();
+    },3000);
 }
 
 
